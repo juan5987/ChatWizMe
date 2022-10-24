fdcbf44069381625299578e3a8bfdfdbd7026489
Juan <juan.machado.5987@gmail.com>
2022-10-24 11:58:19 +0200

    working on AI response

--- ChatWizMe/chatbox/views.py
+++ ChatWizMe/chatbox/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from . import models
 from . import forms
+import os
+import openai
+from dotenv import load_dotenv
 
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
+openai.api_key = SECRET_KEY
 
 user_messages = []
 bot_messages = []
@@ -15,11 +21,23 @@ def chatbox(request):
         if form.is_valid() :
             new_msg = form.cleaned_data['text_msg']
             user_messages.append(new_msg)
-            print(user_messages)
+            response = openai.Completion.create(
+                model="text-davinci-002",
+                prompt=f"The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: {user_messages[len(user_messages) - 1]}\nAI: ",
+                temperature=0.9,
+                max_tokens=150,
+                top_p=1,
+                frequency_penalty=0.0,
+                presence_penalty=0.6,
+                stop=[" Human:", " AI:"]
+                )
+            bot_messages.append(response['choices'][0]['text'])
             form.save()
             form = forms.InputForm()
-            return render(request, "chatbox/chatbox.html", {'form': form, 'user_messages': user_messages})
+            return render(request, "chatbox/chatbox.html", {'form': form, 'user_messages': user_messages, 'bot_messages': bot_messages})
+        
         else:
             return render(request, "chatbox/chatbox.html", {'form': form})
+        
     else:
         return render(request, "chatbox/chatbox.html", {'form': form})
