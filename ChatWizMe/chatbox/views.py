469f03e084d6dcff0b1f384ec1a70d461f18031b
Juan <juan.machado.5987@gmail.com>
2022-10-26 16:58:59 +0200

    added correction

--- ChatWizMe/chatbox/views.py
+++ ChatWizMe/chatbox/views.py
@@ -13,52 +13,63 @@ user_messages = []
 bot_messages = []
 
 messages = {}
-
+correction = []
 
 def chatbox(request):
     form = forms.InputForm(request.POST or None)
     
-    new_msg = None
+    user_msg = None
     
     if request.method == "POST" :
         if form.is_valid() :
-            new_msg = form.cleaned_data['text_msg']
-            user_messages.append(new_msg)
+            user_msg = form.cleaned_data['text_msg']
+            user_messages.append(user_msg)
             form.save()
             form = forms.InputForm()
             response = openai.Completion.create(
                 model="text-davinci-002",
-                prompt=f"\n\nHuman: {user_messages[len(user_messages) - 1]}\nAI: ",
-                temperature=0.9,
-                max_tokens=150,
-                top_p=1,
+                prompt=f"Correct this to standard English:{user_msg}",
+                temperature=0,
+                max_tokens=60,
+                top_p=1.0,
                 frequency_penalty=0.0,
-                presence_penalty=0.6,
-                stop=[" Human:", " AI:"]
+                presence_penalty=0.0
                 )
-            bot_messages.append(response['choices'][0]['text'])
-            time = datetime.today().strftime('%H:%M')            
-            messages[user_messages[len(user_messages) - 1]] = bot_messages[len(bot_messages) - 1]
-            return render(request, "chatbox/chatbox.html", {'form': form, 'messages':messages, 'time': time})
+            normalized_user_message = user_msg.replace(" ", "").lower()
+            normalized_bot_message = response['choices'][0]['text'].replace(" ", "").replace(",", "").lower()[2:]
+            
+            # final_bot_message = "Your sentence is incorrect. It should be as follow:" + "\n\n" + response['choices'][0]['text'] + "\n\n" + "\n\n" "Try to write it again to improve your skill."
+            
+            message1 = "Your sentence is incorrect. It should be as follow:"
+            message2 = response['choices'][0]['text']
+            message3 = "Try to write it again to improve your skill."
+            
+            if normalized_user_message == normalized_bot_message:
+                
+                response = openai.Completion.create(
+                    model="text-davinci-002",
+                    prompt=f"You: {user_msg}\nFriend:",
+                    temperature=0.5,
+                    max_tokens=600,
+                    top_p=1.0,
+                    frequency_penalty=0.5,
+                    presence_penalty=0.0,
+                    stop=["You:"]
+                    )
+                final_bot_message = response['choices'][0]['text']
+                bot_messages.append(final_bot_message)
+                time = datetime.today().strftime('%H:%M')
+                messages[user_messages[len(user_messages) - 1]] = bot_messages[len(bot_messages) - 1]
+                return render(request, "chatbox/chatbox.html", {'form': form, 'messages':messages, 'time': time})
+                
+            else:
+                correction = [message1, message2, message3]
+                time = datetime.today().strftime('%H:%M')         
+                messages[user_messages[len(user_messages) - 1]] = ""
+                return render(request, "chatbox/chatbox.html", {'form': form, 'messages':messages, 'time': time, 'correction' : correction})
         
         else:
             return render(request, "chatbox/chatbox.html", {'form': form})
         
     else:
         return render(request, "chatbox/chatbox.html", {'form': form})
-    
-
-def api(request):
-    response = openai.Completion.create(
-                model="text-davinci-002",
-                prompt=f"The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: {user_messages[len(user_messages) - 1]}\nAI: ",
-                temperature=0.9,
-                max_tokens=150,
-                top_p=1,
-                frequency_penalty=0.0,
-                presence_penalty=0.6,
-                stop=[" Human:", " AI:"]
-                )
-    bot_messages.append(response['choices'][0]['text'])
-    messages[user_messages[len(user_messages) - 1]] = bot_messages[len(bot_messages) - 1]
-    return render(request, "chatbox/chatbox.html", {'messages':messages})
