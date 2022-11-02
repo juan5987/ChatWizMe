1144ec48d37fe473caf558813d53a4906ec75f49
Juan <juan.machado.5987@gmail.com>
2022-11-02 12:26:49 +0100

    added signup logic and login button

--- ChatWizMe/chatbox/views.py
+++ ChatWizMe/chatbox/views.py
@@ -11,112 +11,74 @@ openai.api_key = SECRET_KEY
 
 messages = []
 
-# def chatbox(request):
-#     form = forms.InputForm(request.POST or None)
-    
-#     user_msg = None
-    
-#     if request.method == "POST" :
-#         if form.is_valid() :
-#             user_msg = form.cleaned_data['text_msg']
-#             user_messages.append(user_msg)
-#             form.save()
-#             form = forms.InputForm()
-#             response = openai.Completion.create(
-#                 model="text-davinci-002",
-#                 prompt=f"Correct this to standard English:{user_msg}",
-#                 temperature=0,
-#                 max_tokens=60,
-#                 top_p=1.0,
-#                 frequency_penalty=0.0,
-#                 presence_penalty=0.0
-#                 )
-#             normalized_user_message = user_msg.replace(" ", "").lower()
-#             normalized_bot_message = response['choices'][0]['text'].replace(" ", "").replace(",", "").lower()[2:]
-            
-#             # final_bot_message = "Your sentence is incorrect. It should be as follow:" + "\n\n" + response['choices'][0]['text'] + "\n\n" + "\n\n" "Try to write it again to improve your skill."
-            
-#             message1 = "Your sentence is incorrect. It should be as follow:"
-#             message2 = response['choices'][0]['text']
-#             message3 = "Try to write it again to improve your skill."
-            
-#             if normalized_user_message == normalized_bot_message:
-                
-#                 response = openai.Completion.create(
-#                     model="text-davinci-002",
-#                     prompt=f"You: {user_msg}\nFriend:",
-#                     temperature=0.5,
-#                     max_tokens=600,
-#                     top_p=1.0,
-#                     frequency_penalty=0.5,
-#                     presence_penalty=0.0,
-#                     stop=["You:"]
-#                     )
-#                 final_bot_message = response['choices'][0]['text']
-#                 bot_messages.append(final_bot_message)
-#                 time = datetime.today().strftime('%H:%M')
-#                 messages[user_messages[len(user_messages) - 1]] = bot_messages[len(bot_messages) - 1]
-#                 return render(request, "chatbox/chatbox.html", {'form': form, 'messages':messages, 'time': time})
-                
-#             else:
-#                 correction = [message1, message2, message3]
-#                 time = datetime.today().strftime('%H:%M')         
-#                 messages[user_messages[len(user_messages) - 1]] = ""
-#                 return render(request, "chatbox/chatbox.html", {'form': form, 'messages':messages, 'time': time, 'correction' : correction})
-        
-#         else:
-#             return render(request, "chatbox/chatbox.html", {'form': form})
-        
-#     else:
-#         return render(request, "chatbox/chatbox.html", {'form': form})
-
-
 def chatbox(request):
     form = forms.InputForm(request.POST or None)
-    user_msg = None        
+    user_msg = None     
+    logged_in_user = request.user   
     
     if form.is_valid() :
         user_msg = form.cleaned_data['text_msg']
-        messages.append({user_msg : ""})
-        form.save()
-        form = forms.InputForm()
-        time = datetime.today().strftime('%H:%M')
-        response = openai.Completion.create(
-                model="text-davinci-002",
-                prompt=f"Correct this to standard English:{user_msg}",
-                temperature=0,
-                max_tokens=60,
-                top_p=1.0,
-                frequency_penalty=0.0,
-                presence_penalty=0.0
-                )
         
-        normalized_user_message = user_msg.replace(" ", "").replace(",", "").lower()
-        normalized_bot_message = response['choices'][0]['text'].replace(" ", "").replace(",", "").lower()[2:]
-        print(normalized_user_message, normalized_bot_message)
-        form = forms.InputForm()
-        
-        if normalized_user_message == normalized_bot_message:
-                
-            response = openai.Completion.create(
-                model="text-davinci-002",
-                prompt=f"You: {user_msg}\nFriend:",
-                temperature=0.5,
-                max_tokens=600,
-                top_p=1.0,
-                frequency_penalty=0.5,
-                presence_penalty=0.0,
-                stop=["You:"]
-                )
-            messages.append({'': response['choices'][0]['text']})
+        if user_msg == "Who is the best teacher in Simplon ?":
+            messages.append({user_msg: "Who is teaching you today, Charles or Jeremy ?"})
+            time = datetime.today().strftime('%H:%M')
+            form = forms.InputForm()
             return render(request, "chatbox/chatbox.html", {'form': form, 'messages':messages, 'time': time})
-
+        elif user_msg == "Jeremy":
+            messages.append({user_msg: "I think Jeremy is the best teacher"})
+            time = datetime.today().strftime('%H:%M')
+            form = forms.InputForm()
+            return render(request, "chatbox/chatbox.html", {'form': form, 'messages':messages, 'time': time})
+        elif user_msg == "Charles":
+            messages.append({user_msg: "I think Charles is the best teacher"})
+            time = datetime.today().strftime('%H:%M')
+            form = forms.InputForm()
+            return render(request, "chatbox/chatbox.html", {'form': form, 'messages':messages, 'time': time})
         else:
-            messages.append({"" : "Your sentence is incorrect. It should be as follow:"})
-            messages.append({"" : response['choices'][0]['text']})
-            messages.append({"" : "Try to write it again to improve your skill."})
-            return render(request, "chatbox/chatbox.html", {'form': form, 'messages':messages, 'time': time})
+            messages.append({user_msg : ""})
+            form.save()
+            form = forms.InputForm()
+            time = datetime.today().strftime('%H:%M')
+            response = openai.Completion.create(
+                    model="text-davinci-002",
+                    prompt=f"Correct this to standard English:{user_msg}",
+                    temperature=0,
+                    max_tokens=60,
+                    top_p=1.0,
+                    frequency_penalty=0.0,
+                    presence_penalty=0.0
+                    )
+            
+            normalized_user_message = user_msg.replace(" ", "").replace(",", "").replace(".", "").lower()
+            normalized_bot_message = response['choices'][0]['text'].replace(" ", "").replace(",", "").replace(".", "").lower()[2:]
+            print(normalized_user_message, normalized_bot_message)
+            form = forms.InputForm()
+            
+            if normalized_user_message == normalized_bot_message:
+                    
+                response = openai.Completion.create(
+                    model="text-davinci-002",
+                    prompt=f"You: {user_msg}\nFriend:",
+                    temperature=0.5,
+                    max_tokens=600,
+                    top_p=1.0,
+                    frequency_penalty=0.5,
+                    presence_penalty=0.0,
+                    stop=["You:"]
+                    )
+                messages.append({'': response['choices'][0]['text']})
+                return render(request, "chatbox/chatbox.html", {'form': form, 'messages':messages, 'time': time})
+
+            else:
+                messages.append({"" : "Your sentence is incorrect. It should be as follow:"})
+                messages.append({"" : response['choices'][0]['text']})
+                messages.append({"" : "Try to write it again to improve your skill."})
+                return render(request, "chatbox/chatbox.html", {'form': form, 'messages':messages, 'time': time})
 
     else:
-        return render(request, "chatbox/chatbox.html", {'form': form, 'messages':messages})
+        if str(logged_in_user) != "AnonymousUser":
+            messages.clear()
+            messages.append({"": f"Hello {logged_in_user}, how are you doing today ?"})
+            
+        return render(request, "chatbox/chatbox.html", {'form': form, 'messages':messages, 'user_name': logged_in_user})
 
