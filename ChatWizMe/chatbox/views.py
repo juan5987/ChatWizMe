baf8a6958925c056bd6d47d4f17496e3f59ec04d
Juan <juan.machado.5987@gmail.com>
2022-11-04 12:55:10 +0100

    added comments

--- ChatWizMe/chatbox/views.py
+++ ChatWizMe/chatbox/views.py
@@ -10,6 +10,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 openai.api_key = SECRET_KEY
 
+# on initialise nos variables
 messages = []
 user_prompt = ""
 
@@ -18,20 +19,21 @@ def chatbox(request):
     user_msg = None     
     logged_in_user = request.user
     
-    if form.is_valid() :
-        user_msg = form.cleaned_data['text_msg']     
-        messages.append({user_msg : ""})
-        form = forms.InputForm()
+    if form.is_valid() : # si l'utilisateur a envoyé un message
+        user_msg = form.cleaned_data['text_msg'] # on récupère sa saisie
+        messages.append({user_msg : ""}) # on ajoute le message de l'user dans la liste messages
         time = datetime.today().strftime('%H:%M')
         
-        response = call_api(user_msg)
+        response = call_api(user_msg) # on appelle la fonction qui appelle l'API
         
+        # on normalise les messages de l'utilisateur et de l'IA
         normalized_user_message = user_msg.replace(" ", "").replace(",", "").replace(".", "").lower()
         normalized_bot_message = response.replace(" ", "").replace(",", "").replace(".", "").lower()[2:]
-        form = forms.InputForm()
+        form = forms.InputForm() # on vide le formulaire
         
-        if normalized_user_message == normalized_bot_message:
+        if normalized_user_message == normalized_bot_message: # si les messages sont identiques donc pas de fautede l'user
             global user_prompt
+            # on ajoute a user_prompt l'historique de la conversation et le nouveau message de l'utilisateur
             user_prompt += f"You: {user_msg}\nFriend:"
             response = openai.Completion.create(
                 model="text-davinci-002",
@@ -43,20 +45,20 @@ def chatbox(request):
                 presence_penalty=0.0,
                 stop=["You:"]
                 )
-            messages.append({'': response['choices'][0]['text']})
-            user_prompt += response['choices'][0]['text']
+            messages.append({'': response['choices'][0]['text']}) # on ajoute la reponse du BOT à la variable messages
+            user_prompt += response['choices'][0]['text'] # on ajoute la réponse du BOT dans l'historique pour la prochaine requete a l'API
             return render(request, "chatbox/chatbox.html", {'form': form, 'messages':messages, 'time': time})
 
-        else:
+        else: # si il y a une faute de grammaire
             messages.append({"" : "Your sentence is incorrect. It should be as follow:"})
-            messages.append({"" : response})
+            messages.append({"" : response}) # réponse du BOT
             messages.append({"" : "Try to write it again to improve your skill."})
             return render(request, "chatbox/chatbox.html", {'form': form, 'messages':messages, 'time': time})
 
     else:
-        if str(logged_in_user) != "AnonymousUser":
+        if str(logged_in_user) != "AnonymousUser": # si l'utilisateur est connecté.
             messages.clear()
-            messages.append({"": f"Hello {logged_in_user}, how are you doing today ?"})
+            messages.append({"": f"Hello {logged_in_user}, nice to see you."})
         else:
             messages.clear()
         return render(request, "chatbox/chatbox.html", {'form': form, 'messages':messages, 'user_name': logged_in_user})
